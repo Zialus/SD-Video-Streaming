009e9c4a0b231d1921cb5bf9466a8fadffa70f83
tiaghoul <tiaguinhomartins_@hotmail.com>
2016-12-14 17:08:39 +0000

    now "working" with multiple clients

--- server_src/Server.cpp
+++ server_src/Server.cpp
@@ -13,6 +13,7 @@
 // C++
 #include <fstream>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 
 // My stuff
 #include "StreamServer.h"
@@ -21,98 +22,94 @@
 using namespace FCUP;
 
 void sendVideoTo(int socket, char* ffmpeg_buffer) {
-	int number_of_written_elements = (int) write(socket, ffmpeg_buffer, 255);
-	if (number_of_written_elements < 0){
-		perror("ERROR writing to socket");
-		exit(1);
-	}
+    int number_of_written_elements = (int) write(socket, ffmpeg_buffer, 63);
+    if (number_of_written_elements < 0){
+        perror("ERROR writing to socket");
+        exit(1);
+    }
 }
 
 int main(int argc, char* argv[])
 {
 
-	if (argc < 4)
-	{
-		fprintf(stderr,"ERROR, you need to provide two arguments: a file with the ffmpeg options, and a port to start ffmpeg on.");
-		exit(1);
-	}
+    if (argc < 4)
+    {
+        fprintf(stderr,"ERROR, you need to provide 3 arguments: a file with the ffmpeg options, a port to start ffmpeg on and a file with commands");
+        exit(1);
+    }
 
-	int status = 0;
-	Ice::CommunicatorPtr ic;
-	try {
-		ic = Ice::initialize(argc, argv);
-		Ice::ObjectPrx base = ic->stringToProxy("Portal:default -p 9999");
-		PortalCommunicationPrx portal = PortalCommunicationPrx::checkedCast(base);
-		if (!portal){
-			throw "Invalid proxy";
-		}
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectPrx base = ic->stringToProxy("Portal:default -p 9999");
+        PortalCommunicationPrx portal = PortalCommunicationPrx::checkedCast(base);
+        if (!portal){
+            throw "Invalid proxy";
+        }
 
-		StreamServerEntry allMyInfo;
+        StreamServerEntry allMyInfo;
 
-		StringSequence keywords = {"basketball","Cavs","indoor","sports"};
-		allMyInfo.keywords = keywords;
-		allMyInfo.name = IceUtil::generateUUID();
+        StringSequence keywords = {"basketball","Cavs","indoor","sports"};
+        allMyInfo.keywords = keywords;
+        allMyInfo.name = IceUtil::generateUUID();
 
-		portal->registerStreamServer(allMyInfo);
+        portal->registerStreamServer(allMyInfo);
 
-		pid_t pid = vfork();
-		if ( pid < 0 ) {
-			perror("fork failed");
-			return 1;
-		}
+        pid_t pid = vfork();
+        if ( pid < 0 ) {
+            perror("fork failed");
+            return 1;
+        }
 
-		if ( pid == 0 ) { // Child process
+        if ( pid == 0 ) { // Child process
 
+            char** strings = NULL;
+            size_t count = 0;
+            char string[1000];
 
-			char** strings = NULL;
-			size_t count = 0;
-			char string[1000];
+            // argv[3] contains txt file with ffmpeg options
+            std::ifstream file(argv[3]);
 
-			// argv[3] contains txt file with ffmpeg options
-			std::ifstream file(argv[3]);
+            if(file.is_open()){
+                file >> string;
+                while (!file.eof() ) {
+                    AddString(&strings, &count, string);
+                    file >> string;
+                }
+                file.close();
+            } else{
+                std::cout << "File could not be opened." << std::endl;
+            }
+            AddString(&strings, &count, NULL);
 
-			if(file.is_open()){
-				file >> string;
-				while (!file.eof() ) {
-					AddString(&strings, &count, string);
-					file >> string;
-				}
-				file.close();
-			} else{
-				std::cout << "File could not be opened." << std::endl;
-			}
-			AddString(&strings, &count, NULL);
+            /* char* argv[200] = {"ffmpeg","-i","/home/tiaghoul/Downloads/Popeye_forPresident_512kb.mp4","-loglevel","warning",
+            "-analyzeduration","500k","-probesize","500k","-r","30","-s","640x360","-c:v","libx264","-preset","ultrafast","-pix_fmt",
+            "yuv420p","-tune","zerolatency","-preset","ultrafast","-b:v","500k","-g","30","-c:a","flac","-profile:a","aac_he","-b:a",
+            "32k","-f","mpegts","tcp://127.0.0.1:10000?listen=1",NULL};*/
 
-			/* char* argv[200] = {"ffmpeg","-i","/home/tiaghoul/Downloads/Popeye_forPresident_512kb.mp4","-loglevel","warning",
-			"-analyzeduration","500k","-probesize","500k","-r","30","-s","640x360","-c:v","libx264","-preset","ultrafast","-pix_fmt",
-			"yuv420p","-tune","zerolatency","-preset","ultrafast","-b:v","500k","-g","30","-c:a","flac","-profile:a","aac_he","-b:a",
-			"32k","-f","mpegts","tcp://127.0.0.1:10000?listen=1",NULL};*/
+            for (int i = 0; strings[i] != NULL; ++i) {
+                printf("|%s|\n",strings[i]);
+            }
+            execvp(strings[0], strings);
 
-			for (int i = 0; strings[i] != NULL; ++i)
-			{
-				printf("|%s|\n",strings[i]);
-			}
-			execvp(strings[0], strings);
-
-		} else { // Parent will only start executing after child calls execvp because we are using vfork()
+        } else { // Parent will only start executing after child calls execvp because we are using vfork()
 
             sleep(1);
 
             int n;
-			int socketToReceiveVideoFD;
-			int numberOfWrittenElements;
+            int socketToReceiveVideoFD;
+            int numberOfWrittenElements;
             struct addrinfo hints, *res, *ressave;
 
             char *portToReceiveVideo;
             char *ffmpegServer;
+            char ffmpegBuffer[64];
 
-			char ffmpegBuffer[256];
-
-			// argv[1] contains name of the server
-			ffmpegServer = argv[1];
+            // argv[1] contains name of the server
+            ffmpegServer = argv[1];
             // argv[2] contains port number for the server
             portToReceiveVideo = argv[2];
-
 
             printf("Video vai ser recebido na porta %s e no adress %s\n", portToReceiveVideo, argv[1]);
 
@@ -123,8 +120,9 @@ int main(int argc, char* argv[])
             hints.ai_socktype=SOCK_STREAM;
             hints.ai_protocol=IPPROTO_TCP;
 
-            if((n=getaddrinfo(ffmpegServer, portToReceiveVideo, &hints, &res))!=0)
+            if((n=getaddrinfo(ffmpegServer, portToReceiveVideo, &hints, &res))!=0) {
                 printf("getaddrinfo error for %s, %s; %s", ffmpegServer, portToReceiveVideo, gai_strerror(n));
+            }
 
             ressave=res;
 
@@ -134,104 +132,101 @@ int main(int argc, char* argv[])
                 if(socketToReceiveVideoFD<0)
                     continue;  /*ignore this returned Ip addr*/
 
-                if(connect(socketToReceiveVideoFD, res->ai_addr, res->ai_addrlen)==0)
-                { printf("connection ok!\n"); /* success*/
+                if(connect(socketToReceiveVideoFD, res->ai_addr, res->ai_addrlen)==0) {
+                    printf("connection ok!\n"); /* success*/
                     break;
-                }
-                else  {
+                } else  {
                     perror("connecting stream socket");
                 }
-
 
             } while ((res=res->ai_next)!= NULL);
 
             freeaddrinfo(ressave);
 
+            printf("Connection to FFMPEG succeded!!");
+            sleep(1);
 
-            printf("DID IT!!!");
-			sleep(2);
+            //--------------SERVER PART-----------------//
 
-			//--------------SERVER PART-----------------//
+            socklen_t client_adress_size;
+            struct sockaddr_in server_address, client_address;
 
-			int server_socket_fd;
-			socklen_t client_adress_size;
-			struct sockaddr_in server_address, client_address;
+            //open socket
+            int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (server_socket_fd < 0){
+                perror("ERROR opening socket");
+                std::exit(1);
+            }
 
-			//open socket
-			server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-			if (server_socket_fd < 0){
-				perror("ERROR opening socket");
-				std::exit(1);
-			}
+            int rc = fcntl(server_socket_fd, F_SETFL, O_NONBLOCK);
+            if(rc<0){
+                perror("fcntl() failed");
+                close(server_socket_fd);
+                exit(-1);
+            }
+            // set all values in server_adress to 0
+            bzero((char *) &server_address, sizeof(server_address));
+            // argv[1] contains port number for the server
+            int port_number = 10066;
+            server_address.sin_family = AF_INET;
+            server_address.sin_port = htons(port_number);
+            server_address.sin_addr.s_addr = INADDR_ANY;
 
-			// set all values in server_adress to 0
-			bzero((char *) &server_address, sizeof(server_address));
-			// argv[1] contains port number for the server
-			int port_number = 10066;
-			server_address.sin_family = AF_INET;
-			server_address.sin_port = htons(port_number);
-			server_address.sin_addr.s_addr = INADDR_ANY;
+            int yes=1;
 
-			int bind_result = bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address));
+            int bind_result = bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address));
+            if ( bind_result < 0 ) {
+                perror("ERROR on binding");
+                close(server_socket_fd);
+                exit(1);
+            }
 
-			if ( bind_result < 0 ){
-				perror("ERROR on binding");
-				exit(1);
-			}
+            rc = listen(server_socket_fd, 32);
+            if (rc < 0) {
+                perror("listen() failed");
+                close(server_socket_fd);
+                exit(1);
+            }
 
-			listen(server_socket_fd,5);
+            std::list<int> socketList;
 
-			std::list<int> socketList;
+            printf("Ready to send to clients!\n");
 
-			client_adress_size = sizeof(client_address);
+            while (true) {
 
-			printf("BULLSHIT\n");
+                int new_socket_fd = accept(server_socket_fd, NULL, NULL);
+                if (new_socket_fd > 0){
+                    socketList.push_back(new_socket_fd);
+                }
 
-			while (true) {
+                numberOfWrittenElements = (int) read(socketToReceiveVideoFD, ffmpegBuffer, 63);
+                if (numberOfWrittenElements < 0){
+                    perror("ERROR reading from socket");
+                    exit(1);
+                } else{
+                    printf("Number -> %d\n", numberOfWrittenElements);
+                }
 
+                for(int socket: socketList){
+                    printf("SOCKET -> %d !!!\n", socket);
+                    sendVideoTo(socket, ffmpegBuffer);
+                }
 
-				int yes=1;
-				if(setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) ==-1){
-					perror("setsockopt");
-					exit(1);
-				}
+            }
 
-				int new_socket_fd = accept(server_socket_fd, (struct sockaddr *) &client_address, &client_adress_size);
-				if (new_socket_fd < 0){
-					perror("ERROR on accept");
-					exit(1);
-				}
+        }
 
-				fcntl(new_socket_fd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
+    } catch (const Ice::Exception& ex) {
+        std::cerr << ex << std::endl;
+        status = 1;
+    } catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        status = 1;
+    }
 
-				socketList.push_back(new_socket_fd);
+    if (ic){
+        ic->destroy();
+    }
 
-				numberOfWrittenElements = (int) read(socketToReceiveVideoFD, ffmpegBuffer, 255);
-				if (numberOfWrittenElements < 0){
-					perror("ERROR reading from socket");
-					exit(1);
-				}
-
-				for(int socket: socketList){
-					printf("SOCKET -> %d !!!\n", socket);
-					sendVideoTo(socket, ffmpegBuffer);
-				}
-
-			}
-
-		}
-
-	} catch (const Ice::Exception& ex) {
-		std::cerr << ex << std::endl;
-		status = 1;
-	} catch (const char* msg) {
-		std::cerr << msg << std::endl;
-		status = 1;
-	}
-
-	if (ic){
-		ic->destroy();
-	}
-
-	return status;
+    return status;
 }
