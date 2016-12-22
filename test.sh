0faed32010a1b58fbd2d5ff2450e754fc2585246
Raul Ferreira <raulmendesferreira@gmail.com>
2016-12-22 16:45:02 +0000

    CMake's rules make more sense now.
    Improvements to test script.

--- test.sh
+++ test.sh
@@ -5,16 +5,20 @@ set -x
 mkdir -p ~/VideoStreamerFCUP_Logs
 
 cd ~/Downloads
-wget -nc 'https://archive.org/download/PopeyeAliBaba/PopeyeAlibaba_512kb.mp4'
+wget -nc 'https://archive.org/download/PopeyeAliBaba/PopeyeAliBaba_512kb.mp4'
 wget -nc 'https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4'
 
 cd -
+
+icebox --Ice.Config=config.icebox &
+
+sleep 1
 
 ./executables/portal 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/portal.txt &
 
 sleep 1
 
-./executables/server localhost 54321 10066 640x360 500k libx264 ~/Downloads/PopeyeAlibaba_512kb.mp4 tcp 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
+./executables/server localhost 54321 10066 640x360 500k libx264 ~/Downloads/PopeyeAliBaba_512kb.mp4 tcp 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
 
 sleep 1
 
@@ -24,9 +28,8 @@ sleep 1
 
 ./executables/client 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/client.txt &
 
-# sleep 4
-# killall portal
-# killall server
-# killall client
-# sleep 2
-# killall ffplay
+ sleep 1
+ killall portal
+ killall server
+ killall icebox
+
