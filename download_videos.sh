7ac7234ef9b802aafac403fdafbc280aab206abb
Raul Ferreira <raulmendesferreira@gmail.com>
2016-12-27 13:58:29 +0000

    Fix for script mess-ups...

--- download_videos.sh
+++ download_videos.sh
@@ -5,4 +5,6 @@ set -x
 cd ~/Downloads
 wget -nc 'https://archive.org/download/PopeyeAliBaba/PopeyeAliBaba_512kb.mp4'
 wget -nc 'https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4'
+wget -nc 'http://media.xiph.org/sintel/sintel_trailer-480-png.tar.gz'
+wget -nc 'http://media.xiph.org/sintel/sintel_trailer-audio.flac'
 cd -
