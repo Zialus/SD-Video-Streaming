803f5c0d89487627f1791cc9e96b94e5bb59093c
Raul Ferreira <raulmendesferreira@gmail.com>
2016-12-30 10:42:05 +0000

    Changing tests to reflect code changes.

--- run_basic_test.sh
+++ run_basic_test.sh
@@ -8,11 +8,13 @@ sleep 1
 
 sleep 1
 
-./executables/server localhost 54321 10066 640x360 500k libx264 ~/Downloads/PopeyeAliBaba_512kb.mp4 tcp what,are,you,doing 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
+./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 --my_port 10066 --ff_port 54321 -k what -k are -k you -doing \
+1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
 
 sleep 1
 
-./executables/server localhost 54322 10067 640x360 500k libx265 ~/Downloads/Popeye_forPresident_512kb.mp4 tcp basketball,videogames,stuff 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
+./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 --my_port 10067 --ff_port 54322 -k basketball -k videogames -k stuff \
+1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
 
 sleep 1
 
