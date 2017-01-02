8ea94634b062f8504b767f48c8f3cff68107489e
Raul Ferreira <raulmendesferreira@gmail.com>
2017-01-02 14:47:41 +0000

    Fixed stuff in test script.

--- run_basic_test.sh
+++ run_basic_test.sh
@@ -8,12 +8,22 @@ sleep 1
 
 sleep 1
 
-./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 --my_port 10066 --ff_port 54321 -k what -k are -k you -k doing \
+./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
+--my_port 10066 --ff_port 54321 -k what -k are -k you -k doing -n alibabaNORMAL \
+1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
+
+./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
+--my_port 10066 --ff_port 54321 -k what -k are -k you -k doing -n alibabaHLS --hls \
 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
 
 sleep 1
 
-./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 --my_port 10067 --ff_port 54322 -k basketball -k videogames -k stuff \
+./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
+--my_port 10067 --ff_port 54322 -k basketball -k videogames -n presidentNORMAL \
+1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
+
+./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
+--my_port 10067 --ff_port 54322 -k basketball -k videogames -n presidentDASH --dash \
 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
 
 sleep 1
@@ -25,3 +35,5 @@ sleep 2
 killall portal
 killall server
 killall icebox
+
+cat ~/VideoStreamerFCUP_Logs/portal.txt
