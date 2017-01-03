31e7922daa56e4ffeeb525aa0b03a90d86f623f5
Raul Ferreira <raulmendesferreira@gmail.com>
2017-01-03 00:20:56 +0000

    Fixed stuff in test script.

--- run_basic_test.sh
+++ run_basic_test.sh
@@ -4,27 +4,27 @@ set -x
 
 sleep 1
 
-./executables/portal 1> /dev/null 2> ~/VideoStreamerFCUP_Logs/portal.txt &
+./executables/portal &> ~/VideoStreamerFCUP_Logs/portal.txt &
 
 sleep 1
 
 ./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
 --my_port 10066 --ff_port 54321 -k what -k are -k you -k doing -n alibabaNORMAL \
-1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
+&> ~/VideoStreamerFCUP_Logs/serverAliBabaNORMAL.txt &
 
 ./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
---my_port 10066 --ff_port 54321 -k what -k are -k you -k doing -n alibabaHLS --hls \
-1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverAliBaba.txt &
+--my_port 10067 --ff_port 54322 -k what -k are -k you -k doing -n alibabaHLS --hls \
+&> ~/VideoStreamerFCUP_Logs/serverAliBabaHLS.txt &
 
 sleep 1
 
 ./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
---my_port 10067 --ff_port 54322 -k basketball -k videogames -n presidentNORMAL \
-1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
+--my_port 10068 --ff_port 54323 -k basketball -k videogames -n presidentNORMAL \
+&> ~/VideoStreamerFCUP_Logs/serverPresidentNORMAL.txt &
 
 ./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
---my_port 10067 --ff_port 54322 -k basketball -k videogames -n presidentDASH --dash \
-1> /dev/null 2> ~/VideoStreamerFCUP_Logs/serverPresident.txt &
+--my_port 10069 --ff_port 54324 -k basketball -k videogames -n presidentDASH --dash \
+&> ~/VideoStreamerFCUP_Logs/serverPresidentDASH.txt &
 
 sleep 1
 
@@ -37,3 +37,7 @@ killall server
 killall icebox
 
 cat ~/VideoStreamerFCUP_Logs/portal.txt
+
+cat ~/VideoStreamerFCUP_Logs/serverAliBabaNORMAL.txt
+
+cat ~/VideoStreamerFCUP_Logs/serverPresidentNORMAL.txt
