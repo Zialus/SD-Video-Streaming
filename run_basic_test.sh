cd98bab517e878ddd5ce2acebca4e0c0bd775f36
Raul Ferreira <raulmendesferreira@gmail.com>
2017-07-21 00:33:03 +0100

    Improvements to test script.

--- run_basic_test.sh
+++ run_basic_test.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-icebox --Ice.Config="ice_config_dir/config.icebox" &
+icebox --Ice.Config="ice_config_dir/config.icebox" &> ~/VideoStreamerFCUP_Logs/icebox.txt &
 
 sleep 2
 
@@ -12,29 +12,46 @@ sleep 2
 
 ./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
 --my_port 10066 --ff_port 54321 -k what -k are -k you -k doing -n alibabaNORMAL \
-&> ~/VideoStreamerFCUP_Logs/serverAliBabaNORMAL.txt --Ice.Config="ice_config_dir/config.server" &
-
-./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
---my_port 10067 --ff_port 54322 -k what -k are -k you -k doing -n alibabaHLS --hls \
-&> ~/VideoStreamerFCUP_Logs/serverAliBabaHLS.txt --Ice.Config="ice_config_dir/config.server" &
-
-./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
---my_port 10068 --ff_port 54323 -k basketball -k videogames -n presidentNORMAL \
-&> ~/VideoStreamerFCUP_Logs/serverPresidentNORMAL.txt --Ice.Config="ice_config_dir/config.server" &
-
-./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
---my_port 10069 --ff_port 54324 -k basketball -k videogames -n presidentDASH --dash \
-&> ~/VideoStreamerFCUP_Logs/serverPresidentDASH.txt --Ice.Config="ice_config_dir/config.server" &
+--Ice.Config="ice_config_dir/config.server" &> ~/VideoStreamerFCUP_Logs/serverAliBabaNORMAL.txt &
 
 sleep 2
 
-./executables/client --Ice.Config="ice_config_dir/config.client" < ./extras/ClientCommands.txt
+./executables/server -f ~/Downloads/PopeyeAliBaba_512kb.mp4 -e libx264 -b 500k -v 640x360 \
+--my_port 10067 --ff_port 54322 -k what -k are -k you -k doing -n alibabaHLS --hls \
+--Ice.Config="ice_config_dir/config.server" &> ~/VideoStreamerFCUP_Logs/serverAliBabaHLS.txt &
+
+sleep 2
+
+./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
+--my_port 10068 --ff_port 54323 -k basketball -k videogames -n presidentNORMAL \
+--Ice.Config="ice_config_dir/config.server" &> ~/VideoStreamerFCUP_Logs/serverPresidentNORMAL.txt &
+
+sleep 2
+
+./executables/server -f ~/Downloads/Popeye_forPresident_512kb.mp4 -e libx265 -b 500k -v 640x360 \
+--my_port 10069 --ff_port 54324 -k basketball -k videogames -n presidentDASH --dash \
+--Ice.Config="ice_config_dir/config.server" &> ~/VideoStreamerFCUP_Logs/serverPresidentDASH.txt &
+
+sleep 2
+
+./executables/client --Ice.Config="ice_config_dir/config.client" < ./extras/ClientCommands.txt \
+&> ~/VideoStreamerFCUP_Logs/client.txt
 
 sleep 2
 
 killall server
+
+sleep 5
+
 killall portal
+
+sleep 5
+
 killall icebox
+
+sleep 2
+
+cat ~/VideoStreamerFCUP_Logs/client.txt
 
 cat ~/VideoStreamerFCUP_Logs/portal.txt
 
@@ -45,3 +62,5 @@ cat ~/VideoStreamerFCUP_Logs/serverAliBabaHLS.txt
 cat ~/VideoStreamerFCUP_Logs/serverPresidentNORMAL.txt
 
 cat ~/VideoStreamerFCUP_Logs/serverPresidentDASH.txt
+
+cat ~/VideoStreamerFCUP_Logs/icebox.txt
